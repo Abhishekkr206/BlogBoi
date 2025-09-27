c8f91bf57797360a291ddb8e3e17f995c6c40cde
Abhishekkr206 <abhishek.krr206@gmail.com>
2025-09-27 23:45:58 +0530

    setup home page

---
+++ frontend/pages/home.jsx
@@ -0,0 +1,11 @@
+import PostCard from "../components/postCard"
+
+export default function Home(){
+    return(
+        <>
+            <div className="bg-red-200">
+                <h1>hi</h1>
+            </div>
+        </>
+    )
+}
